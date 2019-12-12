432aa973a0673fb398dae11198d05f3bef46c550
io <io@DESKTOP-DP9DE2D>
2019-12-12 20:27:03 +0100

    ejercicios

---
+++ src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,14 @@
+package boletin9_5;
+
+
+
+import java.util.Scanner;
+
+ class Boletin9_5 {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        System.out.println("Escribe un numero");
+    }
+    
+}
